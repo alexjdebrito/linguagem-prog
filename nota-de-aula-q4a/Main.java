9167bda84e7627b9756815d437f1f357cc3e0892
alexjdebrito <alexjdebrito@gmail.com>
2025-10-08 09:11:51 -0300

    Update Main.java

--- nota-de-aula-q4a/Main.java
+++ nota-de-aula-q4a/Main.java
@@ -1,1 +1,13 @@
+package questao4a;
 
+public class Main {
+    public static void main(String[] args) {
+        ClinicoGeral clinico = new ClinicoGeral("Dr. João", "12345", true, true);
+        clinico.tratarPaciente();
+        clinico.receitar();
+
+        Cirurgiao cirurgiao = new Cirurgiao("Dra. Maria", "67890", true);
+        cirurgiao.tratarPaciente();
+        cirurgiao.fazerCirurgia();
+    }
+}
